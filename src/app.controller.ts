e5c90c85a09df9abc73ed27ed6bc13877f984837
Airton Franco <airton.silveira@rvmdigital.com.br>
2023-02-14 18:10:38 -0300

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,44 +3,12 @@ import { Body, Res, HttpCode, Param, Post, Patch, Delete } from '@nestjs/common/
 import { response } from 'express';
 import { AppService } from './app.service';
 
-@Controller('courses')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  findAll() {
-    return 'Listagem de cursos'
-  }
-
-  @Get(':name')
-  findName(@Param() param) {
-    return `Curso : ${param.name}`
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return `Curso : ${id}`
-  }
-
-  @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
-  create(@Body('name') body) {
-    return body
-  }
-
-  //Função com retorno de status code fixo
-  @Get('/unidade/:res')
-  findRes(@Res() response, @Param() param) {
-    return response.status(400).send(`Unidade: ${param.res}`)
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return `Atualização do Curso : ${id}`
-  }
-
-  @Delete(':id')
-  delete(@Param('id') id: string, @Body() body) {
-    return `Exclusão do Curso : ${id}`
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,20 +1,46 @@
-import { Controller } from '@nestjs/common';
-import { Get, Param } from '@nestjs/common/decorators';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Body, Res, HttpCode, Param, Post, Patch, Delete } from '@nestjs/common/decorators';
+import { response } from 'express';
+//import { AppService } from './app.service';
 
 @Controller('courses')
 export class CoursesController {
-    @Get("list")
-    findAll() {
-        return "Lista de Cursos"
-    }
-    //1° Exemplo de implementação
-    @Get("id/:id")
-    findOne(@Param() paramns) {
-        return `Curso #${paramns.id}`
-    }
-    //2° Exemplo de implementação
-    @Get("name/:name")
-    findOneCourseName(@Param('name') name: string) {
-        return `Curso: ${name}`
-    }
+  //constructor(private readonly appService: AppService) {}
+
+  @Get()
+  findAll() {
+    return 'Listagem de cursos'
+  }
+
+  @Get(':name')
+  findName(@Param() param) {
+    return `Curso : ${param.name}`
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return `Curso : ${id}`
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body('name') body) {
+    return body
+  }
+
+  //Função com retorno de status code fixo
+  @Get('/unidade/:res')
+  findRes(@Res() response, @Param() param) {
+    return response.status(400).send(`Unidade: ${param.res}`)
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `Atualização do Curso : ${id}`
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string, @Body() body) {
+    return `Exclusão do Curso : ${id}`
+  }
 }
